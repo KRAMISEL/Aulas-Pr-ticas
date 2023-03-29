41ef7474c6d77afbb45e2ba41af40b762536e7cc
KRAMISEL <deividssantos210@gmail.com>
2023-03-29 12:33:19 -0300

    .

---
+++ atividade2.cs
@@ -0,0 +1,9 @@
+using System;
+class program{
+    static void Main(){
+        Console.Write("Por favor, digite o seu nome. ");
+        string nome = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Olá " + nome + " seja muito bem vindo.");
+    }
+}
