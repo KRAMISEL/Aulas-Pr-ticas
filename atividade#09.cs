68a6d256df3dd23157e1e6b47dfd625a90fd440d
KRAMISEL <deividssantos210@gmail.com>
2023-04-05 12:15:42 -0300

    Real.

--- atividade#09.cs
+++ atividade#09.cs
@@ -3,6 +3,6 @@ class program{
     static void Main(){
         Console.WriteLine("Me diga quantos Reais você tem. ");
         double Dinheiro = double.Parse(Console.ReadLine());
-        Console.WriteLine("Você pode comprar D$" + Dinheiro * 0.29);
+        Console.WriteLine("Você pode comprar D$" + Dinheiro / 3.45);
     }
 }
