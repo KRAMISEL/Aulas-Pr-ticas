e7fbdcce1917040a5459fedf1ea0d73de0f22b1e
KRAMISEL <deividssantos210@gmail.com>
2023-04-15 16:24:16 -0300

    Real.

--- atividade#10.cs
+++ atividade#10.cs
@@ -5,5 +5,6 @@ Console.Write("Me diga qual é a largura e a altura da parede que deseja pintar:
         double Largura = double.Parse(Console.ReadLine());
         double Altura = double.Parse(Console.ReadLine());
         Console.WriteLine("A área da parede a ser pintada é " + Largura * Altura + " metros quadrados, e você precisará de " + Largura * Altura / 2 + " litros de tinta para isso.");
+        Console.WriteLine("A área da parede a ser pintada é {0} metros quadrados, e você precisará de {1} litros de tinta para isso.",Largura*Altura,Largura*Altura/2);
     }
 }
