8c4d93336a0a843c33cb51841b94ae8a13a7c92a
KRAMISEL <deividssantos210@gmail.com>
2023-05-08 20:58:04 -0300

    .

--- classes.cs
+++ classes.cs
@@ -15,5 +15,21 @@ public class Mostarclasses{
         Console.WriteLine();
         Console.WriteLine(MÉ.métodos);
         Console.WriteLine();
+        /*
+        Modificador da Classe: Define a visibilidade da classe.
+            public: Pública, sem restrição de visualização.
+            abstract: Classe-Base para outras classes, não pode ser instanciados objetos desta classe.
+            sealed: Classe não pode ser herdada.
+            static: Classe não permite a instanciação de objetos e seus membros devem ser static.
+
+        Especificador de Classe: Onde um membro da classe pode ser acessado.
+            public: sem restrição de acesso.
+            private: só podem ser acessados pela própria classe.
+            protected: Podem ser acessados na própria classe e nas classes derivadas.
+            abstract: Os métodos não tem implementação somente os cabeçalhos.
+            sealed: O método não pode ser redefinido.
+            virtual: O método pode ser redefinido em uma classe derivada.
+            static: O Método pode ser chamado sem a instanciação de um objeto.
+        */
     }
 }
