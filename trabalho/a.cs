18610092b2f6ba702d675247195a8347db501f09
KRAMISEL <deividssantos210@gmail.com>
2023-05-09 16:25:08 -0300

    .

--- trabalho/a.cs
+++ trabalho/a.cs
@@ -1,57 +1,51 @@
 using System;
 class program{
+
+    static char Op;
     static int quantidadeDeCadastro;
     static int[] quantidadeDeCadastro1;
     static int A;
     static int N_deCadastros;
     static void Main(){
-        Console.WriteLine("A");
-        Console.ReadLine();
-        Cadastrar();
-    }
-    static void Cadastrar(){
-        if(quantidadeDeCadastro > 1){
-            Novoscadastros();
-            goto avançar;
-        }
-        Console.Write("Quantos cadastros deseja fazer: ");
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Console.WriteLine("Quantos cadastros deseja fazer: ");
         quantidadeDeCadastro = int.Parse(Console.ReadLine());
         quantidadeDeCadastro1 = new int[quantidadeDeCadastro];
+        Cadastrar();
+        Console.WriteLine("deseja fazer um novo cadastro? [s/n]");
+        Op = char.Parse(Console.ReadLine());
+        if(Op == 's'){
+            Novoscadastros();
+        }
+    }
+    static void Cadastrar(){
         for(A = 0; A < quantidadeDeCadastro1.Length; A++){
         if(N_deCadastros < A){
             N_deCadastros++;
         }
-            Console.WriteLine("({0})",N_deCadastros + 1);
-            Console.Write(": ");
+            Console.WriteLine("{0} :",N_deCadastros + 1);
             quantidadeDeCadastro1[A] = int.Parse(Console.ReadLine());
             Console.WriteLine();
         }
-        N_deCadastros -= A;
+        N_deCadastros = 0;
         for(A = 0; A < quantidadeDeCadastro1.Length; A++){
         if(N_deCadastros < A){
             N_deCadastros++;
         }
-            Console.WriteLine("({0})",N_deCadastros + 1);
+            Console.WriteLine("{0}",N_deCadastros + 1);
             Console.WriteLine(quantidadeDeCadastro1[A]);
         }
-        avançar:
-        Main();
     }
     static void Novoscadastros(){
         Console.Write("Quantos cadastros deseja fazer: ");
-        int quantidadeDeCadastro2 = int.Parse(Console.ReadLine());
-        int Aa = quantidadeDeCadastro1.Length + quantidadeDeCadastro2;
-        for(A = Aa; A < Aa - quantidadeDeCadastro2; A++){
-        if(N_deCadastros < A){
-            N_deCadastros++;
-        }
-            Console.WriteLine("({0})",N_deCadastros + 1);
-            Console.Write(": ");
-            quantidadeDeCadastro1[A] = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-        }
-        N_deCadastros -= A;
-        for(A = 0; A < Aa; A++){
+        quantidadeDeCadastro = int.Parse(Console.ReadLine());
+    
+        Console.WriteLine("{0} :",N_deCadastros + 1);
+        quantidadeDeCadastro1[quantidadeDeCadastro] = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        
+        N_deCadastros = 0;
+        for(A = 0; A < quantidadeDeCadastro1.Length; A++){
         if(N_deCadastros < A){
             N_deCadastros++;
         }
