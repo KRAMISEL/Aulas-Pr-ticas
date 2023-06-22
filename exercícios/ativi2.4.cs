2a981f3154986d50d1467cb8f86e7c029151b6fc
KRAMISEL <deividssantos210@gmail.com>
2023-06-21 23:07:08 -0300

    .

--- exercícios/ativi2.4.cs
+++ exercícios/ativi2.4.cs
@@ -2,6 +2,8 @@ using System;
 class Programa{
     static void Main(){
         int a = 4;
+        int C = 0;
+        int D = 0;
         int[,] números = new int[a,a];
         Console.WriteLine("Digite números:");
         for(int A = 0; A < a; A++){
@@ -16,6 +18,38 @@ class Programa{
             }
             Console.WriteLine();
         }
+        Console.WriteLine("====================>");
+        Console.WriteLine("====================>\n");
+        Console.WriteLine("Números pares:");
+        for(int A = 0; A < a; A++){
+            for(int B = 0; B < a; B++){
+                if(números[A,B] % 2 == 0){
+                    Console.Write(" {0} ",números[A,B]);
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("====================>\n");
+        Console.WriteLine("Números ímpares:");
+        for(int A = 0; A < a; A++){
+            for(int B = 0; B < a; B++){
+                if(números[A,B] % 2 != 0){
+                    Console.Write(" {0} ",números[A,B]);
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("====================>\n");
+        Console.WriteLine("Média dos números ímpares:");
+        for(int A = 0; A < a; A++){
+            for(int B = 0; B < a; B++){
+                if(números[A,B] % 2 != 0){
+                    C += números[A,B];
+                    D++;
+                }
+            }
+        }
+        Console.WriteLine(C / D);
         Console.WriteLine("====================>");
     }
 }
