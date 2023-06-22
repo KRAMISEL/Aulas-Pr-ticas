87d41b4e9da146bfafe13f5d0630f664dfd44fa6
KRAMISEL <deividssantos210@gmail.com>
2023-06-22 17:53:16 -0300

    .

--- trabalho/sopradis1.cs
+++ trabalho/sopradis1.cs
@@ -11,13 +11,10 @@ class Números{
             goto voltar;
         }
         float[] númaleatório = new float[A];
-        float[] númordenados = new float[A];
         Console.WriteLine("Agora digite quais os números que deseja mostrar:");
         for(int i = 0; i < númaleatório.Length; i++){
             númaleatório[i] = float.Parse(Console.ReadLine());
-            númordenados[i] = númaleatório[i];
         }
-        Array.Sort(númordenados);
         Console.WriteLine("\nO primeiro número da coleção é {0}.\nO ultimo número da coleção é {1}.",númaleatório[0],númaleatório[0 + númaleatório.Length - 1]);
         for(int i = 0; i < númaleatório.Length - 1; i++){
             B = i;
@@ -35,14 +32,15 @@ class Números{
             }
         }
         Console.WriteLine("\nSendo que a coleção possui {0} números pares e {1} números ímpares.",C,D);
-        Console.WriteLine("\nO menor número sendo {0}.\nO maior número sendo {1}.\n",númordenados[0],númordenados[0 + númordenados.Length - 1]);
+        Array.Sort(númaleatório);
+        Console.WriteLine("\nO menor número sendo {0}.\nO maior número sendo {1}.\n",númaleatório[0],númaleatório[0 + númaleatório.Length - 1]);
         Console.WriteLine("A coleção ordenada do menor para o maior:");
-        foreach(int i in númordenados){
+        foreach(int i in númaleatório){
             Console.WriteLine(i);
         }
         Console.WriteLine("\nA coleção ordenada do maior para o menor:");
-        Array.Reverse(númordenados);
-        foreach(int i in númordenados){
+        Array.Reverse(númaleatório);
+        foreach(int i in númaleatório){
             Console.WriteLine(i);
         }
     }
