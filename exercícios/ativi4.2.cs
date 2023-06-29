0b127c88f059f73697471a63284ae2f78ed9995d
KRAMISEL <deividssantos210@gmail.com>
2023-06-29 17:21:44 -0300

    .

--- exercícios/ativi4.2.cs
+++ exercícios/ativi4.2.cs
@@ -4,15 +4,15 @@ class vetor{
         int A = 5;
         float[] núm = new float[A * 2];
         Console.WriteLine("Preencha o vetor com números.");
-        for(int i = 0; i < núm.Length / 2; i++){
+        for(int i = 0; i < A; i++){
             núm[i] = int.Parse(Console.ReadLine());
-            núm[i + núm.Length / 2] = núm[i];
+            núm[i + A] = núm[i];
         }
-        for(int i = 0; i < núm.Length / 2; i++){
-            núm[(A - 1) - i] = núm[i + núm.Length / 2];
+        for(int i = 0; i < A; i++){
+            núm[(A - 1) - i] = núm[i + A];
         }
         Console.Write("\n=========");
-        for(int i = 0; i < núm.Length / 2; i++){
+        for(int i = 0; i < A; i++){
             Console.WriteLine("\n{0}",núm[i]);
         }
         Console.WriteLine("=========");
