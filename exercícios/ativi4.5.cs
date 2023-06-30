39a27a43573610a66b3e44a1e9eeff367f39a9c2
KRAMISEL <deividssantos210@gmail.com>
2023-06-29 21:52:36 -0300

    .

---
+++ exercícios/ativi4.5.cs
@@ -0,0 +1,21 @@
+using System;
+class vetor{
+    static void Main(){
+        int A = 5,B;
+        int[] núm = new int[A];
+        Console.WriteLine("Preencha o vetor com números.");
+        for(int i = 0; i < A; i++){
+            núm[i] = int.Parse(Console.ReadLine());
+        }
+        for(int i = 0; i < núm.Length / 2; i++){
+            B = núm[i];
+            núm[i] = núm[(A - 1) - i];
+            núm[(A - 1) - i] = B;
+        }
+        Console.Write("\n=========");
+        foreach(int i in núm){
+            Console.WriteLine("\n{0}",i);
+        }
+        Console.WriteLine("=========");
+    }
+}
