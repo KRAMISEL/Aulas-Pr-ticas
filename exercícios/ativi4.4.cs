f1d7e8a8a6cc89af2ba98cde305becee395adb5b
KRAMISEL <deividssantos210@gmail.com>
2023-07-01 02:30:56 -0300

    .

--- exercícios/ativi4.4.cs
+++ exercícios/ativi4.4.cs
@@ -1,31 +1,31 @@
 using System;
 class vetor{
     static void Main(){
-        int A = 3;
-        float[,] núm = new float[A,A],núm1 = new float[A,A];
+        int A = 3,B = 2;
+        float[,] núm = new float[A,B],núm1 = new float[A,B];
         Console.WriteLine("Preencha o vetor com números.");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+            for(int ia = 0; ia < B; ia++){
                 núm[i,ia] = float.Parse(Console.ReadLine());
                 núm1[i,ia] = núm[i,ia];
             }
         }
         Console.Write("\nOrdem inserida.:\n=========\n");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+            for(int ia = 0; ia < B; ia++){
                 Console.Write(" {0}",núm[i,ia]);
             }
             Console.WriteLine();
         }
         Console.WriteLine("=========");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
-                núm[i,ia] = núm1[(A - 1) - i,(A - 1) - ia];
+            for(int ia = 0; ia < B; ia++){
+                núm[i,ia] = núm1[(A - 1) - i,(B - 1) - ia];
             }
         }
         Console.Write("\nOrdem invertida.:\n=========\n");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+            for(int ia = 0; ia < B; ia++){
                 Console.Write(" {0}",núm[i,ia]);
             }
             Console.WriteLine();
