3b9ce00e3c04a77616d86ba16714fb4c4c39c11e
KRAMISEL <deividssantos210@gmail.com>
2023-07-01 02:27:51 -0300

    .

--- exercícios/ativi4.5.cs
+++ exercícios/ativi4.5.cs
@@ -1,31 +1,31 @@
 using System;
 class vetor{
     static void Main(){
-        int A = 3;
-        float[,] núm = new float[A * 2,A * 2];
+        int A = 3,B = 2;
+        float[,] núm = new float[A * 2,B * 2];
         Console.WriteLine("Preencha a matriz com números.");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+            for(int ia = 0; ia < B; ia++){
                 núm[i,ia] = float.Parse(Console.ReadLine());
-                núm[i + 2,ia + 2] = núm[i,ia];
+                núm[(A + (A - 1)) - i,(B + (B - 1)) - ia] = núm[i,ia];
             }
         }
         Console.Write("\nOrdem inserida.:\n=========\n");
-        for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+        for(int i = 0; i < A * 2; i++){
+            for(int ia = 0; ia < B * 2; ia++){
                 Console.Write(" {0}",núm[i,ia]);
             }
             Console.WriteLine();
         }
         Console.WriteLine("=========");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
-                núm[(A - 1) - i,(A - 1) - ia] = núm[i + 2,ia + 2];
+            for(int ia = 0; ia < B; ia++){
+                núm[i,ia] = núm[i + A,ia + B];
             }
         }
         Console.Write("\nOrdem invertida.:\n=========\n");
         for(int i = 0; i < A; i++){
-            for(int ia = 0; ia < A; ia++){
+            for(int ia = 0; ia < B; ia++){
                 Console.Write(" {0}",núm[i,ia]);
             }
             Console.WriteLine();
