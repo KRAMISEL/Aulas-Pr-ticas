4f3e392691a84ca80f6c64bf2ee627039085bc2e
KRAMISEL <deividssantos210@gmail.com>
2023-07-17 17:55:17 -0300

    .

--- c#/exercícios/ativi3.1.cs
+++ c#/exercícios/ativi3.1.cs
@@ -1,12 +1,9 @@
 using System;
 class Programa{
     static void Main(){
-        int núm1,núm2,núm3;
         voltar:
         Console.WriteLine("Por favor, digite três medidas em centímetros, de 10 a 100, para formar um triangulo:");
-        núm1 = int.Parse(Console.ReadLine());
-        núm2 = int.Parse(Console.ReadLine());
-        núm3 = int.Parse(Console.ReadLine());
+        int núm1 = int.Parse(Console.ReadLine()), núm2 = int.Parse(Console.ReadLine()), núm3 = int.Parse(Console.ReadLine());
         if(núm1 > 100 || núm1 < 10 || núm2 > 100 || núm2 < 10 || núm3 > 100 || núm3 < 10){
             Console.WriteLine("\nMedidas erradas, só são validas medidas de 10 a 100 centímetros.\n");
             Console.ReadLine();
