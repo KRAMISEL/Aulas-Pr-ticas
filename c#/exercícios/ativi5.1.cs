181f6a7b369fa2ae8166f38db4ebf7ee980fbca2
KRAMISEL <deividssantos210@gmail.com>
2023-07-20 06:30:13 -0300

    .

--- c#/exercícios/ativi5.1.cs
+++ c#/exercícios/ativi5.1.cs
@@ -1,46 +1,27 @@
 using System;
 class Matriz{
     static void Main(){
-        int A = 3, B = 3, C = 0, D = 0;
+        int A = 3, B = 3;
+        float númMai = -1000000, mai = -1000000, númMen = 1000000, men = 1000000;
         float[,] matriz = new float[A, B];
-        float núm = 0, núm1 = 0;
-        Console.WriteLine("Digite números na matriz.");
+        Console.WriteLine("Digite números até completar a matriz.");
         for(int i = 0; i < A; i++){
             for(int ia = 0; ia < B; ia++){
-                matriz[i,ia] = float.Parse(Console.ReadLine());
-                if(matriz[i, ia] > matriz[i - C, ia - D]){
-                    núm = matriz[i, ia];
-                }
-                if(D == 0){
-                    D += 1;
-                }
-                if(C == 1){
-                    C -= 1;
-                }
-            }
-            D -= 1;
-            if(C == 0){
-                C += 1;
+                matriz[i, ia] = float.Parse(Console.ReadLine());
             }
         }
-        C -= 1;
         for(int i = 0; i < A; i++){
             for(int ia = 0; ia < B; ia++){
-                if(matriz[i, ia] < matriz[i - C, ia - D]){
-                    núm1 = matriz[i, ia];
+                if(matriz[i, ia] > númMai && matriz[i, ia] > mai){
+                    mai = matriz[i, ia];
                 }
-                if(D == 0){
-                    D += 1;
+                númMai = matriz[i, ia];
+                if(matriz[i, ia] < númMen && matriz[i, ia] < men){
+                    men = matriz[i, ia];
                 }
-                if(C == 1){
-                    C -= 1;
-                }
-            }
-            D -= 1;
-            if(C == 0){
-                C += 1;
+                númMen = matriz[i, ia];
             }
         }
-        Console.WriteLine("Maior número da matriz: {0}.\nMenor número da matriz: {1}.",núm,núm1);
+        Console.WriteLine("O Maior número da matriz é {0}.\nO menor número da matriz é {1}.",mai,men);
     }
 }
