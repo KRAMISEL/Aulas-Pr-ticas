e73a566e4887fd8cfac226186d91bd29b7ad897f
KRAMISEL <deividssantos210@gmail.com>
2023-10-07 20:44:55 -0300

    .

--- javascript/estudos.js
+++ javascript/estudos.js
@@ -1,7 +1,18 @@
 "use Strict"
-let f = "";
+let f = ""
+function* Geradora(){
+    const cor1 = yield 'A cor1 será vermelho'
+    const cor2 = yield 'A cor2 será verde'
+    const cor3 = yield 'A cor3 será azul'
+    return 'Cor1: ' + cor1 + '\nCor2: ' + cor2 + '\nCor3: ' + cor3
+}
 let Função = F => {
     variável = "Olá Mundo"
-    return variável;
+    return variável
 }
-console.log(f = Função());
+console.log(f = Função())
+f = Geradora()
+console.log(f.next().value)
+console.log(f.next('vermelho').value)
+console.log(f.next('verde').value)
+console.log(f.next('azul').value)
